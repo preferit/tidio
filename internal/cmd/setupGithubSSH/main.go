3f9ac52b9fdf35bc6bb117aecb50a5d83c5b656b
Gregory Vincic <g@7de.se>
2020-06-01 14:16:56 +0200

    Use id_rsa

--- internal/cmd/setupGithubSSH/main.go
+++ internal/cmd/setupGithubSSH/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	sshDir := path.Join(os.Getenv("HOME"), ".ssh")
-	filename := path.Join(sshDir, "linode_rsa")
+	filename := path.Join(sshDir, "id_rsa")
 	key := os.Getenv("LINODE_PRIVATE_KEY") // secret on github
 	if key == "" {
 		fmt.Println("LINODE_PRIVATE_KEY env not found")
@@ -22,7 +22,7 @@ func main() {
 	}
 	// hostkey
 	knownHosts := path.Join(sshDir, "known_hosts")
-	err = ioutil.WriteFile(knownHosts, []byte("ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDynhw46Nrnt7FsBg/dAGTCd1LOZTphHo0nPWidmpY/Kr/mdng/VnILpGmQa7fAlv6N9PKKm2kEUvNdnsJDLzjZch4cNLFr8Tql7k4evLBIJq7LHt6Twpc1heH6s1CGDbTZQlWDZhm/vE0jwZGH/3rjlweYQILtItMT3q6m6OQjkeLldkN5KBjHG8Fr73ucrBDc0w4ENcM7cyFYKDU8bMG2oPg86u6v0guQFgTfUydUh88ekbuIHJGvAankgrcDjnEKx2tuVBwxFyWe+Z0Q7UJW5CZVMM1ip10OQgH0CzK174reIxX2MsA0IMTWXMGsuCOJ8cBZzQqtELfrW8EunsQz root@localhost"), 0600)
+	err = ioutil.WriteFile(knownHosts, []byte("tidio.preferit.se ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDynhw46Nrnt7FsBg/dAGTCd1LOZTphHo0nPWidmpY/Kr/mdng/VnILpGmQa7fAlv6N9PKKm2kEUvNdnsJDLzjZch4cNLFr8Tql7k4evLBIJq7LHt6Twpc1heH6s1CGDbTZQlWDZhm/vE0jwZGH/3rjlweYQILtItMT3q6m6OQjkeLldkN5KBjHG8Fr73ucrBDc0w4ENcM7cyFYKDU8bMG2oPg86u6v0guQFgTfUydUh88ekbuIHJGvAankgrcDjnEKx2tuVBwxFyWe+Z0Q7UJW5CZVMM1ip10OQgH0CzK174reIxX2MsA0IMTWXMGsuCOJ8cBZzQqtELfrW8EunsQz root@localhost"), 0600)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
