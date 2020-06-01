75601a7f46a643d17268c4ecf9d44a31f61e5672
Gregory Vincic <g@7de.se>
2020-06-01 14:22:44 +0200

    Include id_rsa.pub

--- internal/cmd/setupGithubSSH/main.go
+++ internal/cmd/setupGithubSSH/main.go
@@ -8,23 +8,39 @@ import (
 )
 
 func main() {
-	sshDir := path.Join(os.Getenv("HOME"), ".ssh")
-	filename := path.Join(sshDir, "id_rsa")
+	// Write ~/.ssh/id_rsa
 	key := os.Getenv("LINODE_PRIVATE_KEY") // secret on github
 	if key == "" {
 		fmt.Println("LINODE_PRIVATE_KEY env not found")
 		os.Exit(1)
 	}
-	err := ioutil.WriteFile(filename, []byte(key), 0600)
+	sshDir := path.Join(os.Getenv("HOME"), ".ssh")
+	err := ioutil.WriteFile(path.Join(sshDir, "id_rsa"), []byte(key), 0600)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	// hostkey
-	knownHosts := path.Join(sshDir, "known_hosts")
-	err = ioutil.WriteFile(knownHosts, []byte("tidio.preferit.se ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDynhw46Nrnt7FsBg/dAGTCd1LOZTphHo0nPWidmpY/Kr/mdng/VnILpGmQa7fAlv6N9PKKm2kEUvNdnsJDLzjZch4cNLFr8Tql7k4evLBIJq7LHt6Twpc1heH6s1CGDbTZQlWDZhm/vE0jwZGH/3rjlweYQILtItMT3q6m6OQjkeLldkN5KBjHG8Fr73ucrBDc0w4ENcM7cyFYKDU8bMG2oPg86u6v0guQFgTfUydUh88ekbuIHJGvAankgrcDjnEKx2tuVBwxFyWe+Z0Q7UJW5CZVMM1ip10OQgH0CzK174reIxX2MsA0IMTWXMGsuCOJ8cBZzQqtELfrW8EunsQz root@localhost"), 0600)
+
+	// Write ~/.ssh/known_hosts
+	err = ioutil.WriteFile(
+		path.Join(sshDir, "known_hosts"),
+		[]byte("tidio.preferit.se ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDynhw46Nrnt7FsBg/dAGTCd1LOZTphHo0nPWidmpY/Kr/mdng/VnILpGmQa7fAlv6N9PKKm2kEUvNdnsJDLzjZch4cNLFr8Tql7k4evLBIJq7LHt6Twpc1heH6s1CGDbTZQlWDZhm/vE0jwZGH/3rjlweYQILtItMT3q6m6OQjkeLldkN5KBjHG8Fr73ucrBDc0w4ENcM7cyFYKDU8bMG2oPg86u6v0guQFgTfUydUh88ekbuIHJGvAankgrcDjnEKx2tuVBwxFyWe+Z0Q7UJW5CZVMM1ip10OQgH0CzK174reIxX2MsA0IMTWXMGsuCOJ8cBZzQqtELfrW8EunsQz root@localhost"),
+		0600,
+	)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
+	// Write ~/.ssh/id_rsa.pub
+	err = ioutil.WriteFile(
+		path.Join(sshDir, "id_rsa.pub"),
+		[]byte("ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD0LBHGJENWuQLVzQ3NTRxmiimyOIO3XtbN+fBEYevC7U2h8xj6ihdhCJ04WnnUskuj2lYDTeg8pDGqPggsfOwkHuw7e7znlKzFo3HPAeoXo51JONykrbFCcokWIIrsQiy2Q8GafBQUQ2iQYvEoZ8Ko2L8nVaoUIuGslyEzU6D1YZj4g/O4/Hud35hB6VVzB7m54YQ2KE4vB99HQocoFbzhDdMFthnkvotAyz/lGyPSDWv622sUyECHhF1P18PyOl5DlQ0Y8/kXKbFWFn9gKhmGAHPa7Gxi3sVIQnsTKWcBFV+jkrcXzDSjdAvKprGdgpTmXbZYlR9XkbVu5KqVDKsj gregory@larnic"),
+		0600,
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 }
