26be37414dbfda06e9803721a77e79aeaa167675
Gregory Vincic <g@7de.se>
2020-06-02 11:08:55 +0200

    Test apikeys loading

--- cmd/tidio/main_test.go
+++ cmd/tidio/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -9,6 +10,31 @@ import (
 )
 
 func Test_cli(t *testing.T) {
+	badFile, err := ioutil.TempFile("", "apikeys")
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Fprint(badFile, "{...") // bad json
+	badFile.Close()
+	defer os.RemoveAll(badFile.Name())
+
+	bad := func(msg string, c *cli) {
+		t.Helper()
+		if err := c.run(); err == nil {
+			t.Error(msg)
+		}
+	}
+	bad("bind not set", &cli{})
+	bad("keysfile not found", &cli{
+		bind:     "1",
+		keysfile: "NO SUCH FILE",
+	})
+	bad("bad formata keysfile", &cli{
+		bind:     "1",
+		keysfile: badFile.Name(),
+	})
+
+	// create correct key file
 	fh, err := ioutil.TempFile("", "apikeys")
 	if err != nil {
 		t.Fatal(err)
@@ -20,25 +46,16 @@ func Test_cli(t *testing.T) {
 	fh.Close()
 	defer os.RemoveAll(fh.Name())
 
-	t.Run("run", func(t *testing.T) {
-		c := &cli{keysfile: fh.Name()}
-		if err := c.run(); err == nil {
-			t.Fail()
-		}
-	})
+	ok := func(c *cli) {
+		t.Helper()
 
-	t.Run("start", func(t *testing.T) {
-		var started bool
-		c := &cli{
-			bind:     ":8080",
-			keysfile: fh.Name(),
-			starter:  func(string, http.Handler) error { started = true; return nil },
-		}
 		if err := c.run(); err != nil {
-			t.Fatal(err)
+			t.Error(err)
 		}
-		if !started {
-			t.Error("starter not called")
-		}
+	}
+	ok(&cli{
+		bind:     ":80",
+		keysfile: fh.Name(),
+		starter:  func(string, http.Handler) error { return nil },
 	})
 }
