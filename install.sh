4192eea64a9587b022b7e0278d170062ade2eac0
Gregory Vincic <g@7de.se>
2020-06-02 08:56:45 +0200

    Install static website

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -e
+
+# tidio service
 systemctl stop tidio
 cp tidio ../bin/tidio
 systemctl start tidio
 
+# nginx configuration
 cp etc/tidio.preferit.se /etc/nginx/sites-available/
 systemctl reload nginx
+
+# static website
+rsync -av website/ /var/www/tidio.preferit.se/
