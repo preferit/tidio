41b07b23c2b5feb90623d64e7e7494c7329ee945
Gregory Vincic <g@7de.se>
2020-06-03 08:57:04 +0200

    Document create timesheet api

--- website/main.go
+++ website/main.go
@@ -25,6 +25,7 @@ type cli struct {
 }
 
 func (c *cli) run() error {
+	os.MkdirAll(c.out, 0755)
 	indexPage().SaveTo(c.out)
 	return nil
 }
@@ -33,7 +34,32 @@ func indexPage() *Page {
 	article := Article(
 		H1("Tidio"),
 		A(Href("/api"), "/api"),
+		H2("Timesheets"),
+		H3("Create or update"),
+		Pre(`POST {host}/api/timesheets/{account}/{yyyymm}.timesheet
+Authorization {key}
+
+-- body contains timesheet --`),
 	)
+
+	return NewPage(
+		"index.html",
+		Html(
+			Head(
+				Style(theme()),
+			),
+			Body(article,
+
+				Footer(
+					"Generated: ",
+					time.Now().Round(time.Second).String(),
+				),
+			),
+		),
+	)
+}
+
+func theme() *CSS {
 	css := NewCSS()
 	css.Style("html, body",
 		"margin: 0 0",
@@ -45,24 +71,12 @@ func indexPage() *Page {
 		"padding: 1em 1em 2em 1em",
 		"min-height: 300",
 	)
+	css.Style("pre",
+		"margin-left: 1em",
+	)
 	css.Style("footer",
 		"border-top: 1px solid #727272",
 		"padding: 0.6em 0.6em",
 	)
-
-	return NewPage(
-		"index.html",
-		Html(
-			Head(
-				Style(css),
-			),
-			Body(article,
-
-				Footer(
-					"Generated: ",
-					time.Now().Round(time.Second).String(),
-				),
-			),
-		),
-	)
+	return css
 }
