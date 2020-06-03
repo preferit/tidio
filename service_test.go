c02c1c7c10be9424b35bd6db644107407a3b93ca
Gregory Vincic <g@7de.se>
2020-06-03 09:58:53 +0200

    Test NewService options

--- service_test.go
+++ service_test.go
@@ -4,9 +4,35 @@ import "testing"
 
 func Test_service(t *testing.T) {
 	store := &Store{}
-	apikeys := APIKeys{}
+	apikeys := APIKeys{
+		"KEY": "john",
+	}
 	service := NewService(apikeys, store)
 	if service == nil {
 		t.Fail()
+	}
+
+	if _, ok := service.IsAuthenticated("KEY"); !ok {
+		t.Error("KEY is in apikeys")
+	}
+
+	if _, ok := service.IsAuthenticated(""); ok {
+		t.Error("empty key ok")
+	}
+	if _, ok := service.IsAuthenticated("not there"); ok {
+		t.Error("wrong key ok")
+	}
+}
+
+func Test_service_options(t *testing.T) {
+	defer catchPanic(t)
+	NewService(1)
+}
+
+func catchPanic(t *testing.T) {
+	e := recover()
+	if e == nil {
+		t.Helper()
+		t.Error("didn't panic")
 	}
 }
