83179738e0381457d0264578dea0cda2b438a8dc
Gregory Vincic <g@7de.se>
2020-06-04 07:41:13 +0200

    Document timesheet file format

--- website/main.go
+++ website/main.go
@@ -32,24 +32,69 @@ func (c *cli) run() error {
 
 func indexPage() *Page {
 	article := Article(
-		H1("Tidio"),
+		H1("tidio"),
 		A(Href("/api"), "/api"),
-		H2("Timesheets"),
-		H3("Create or update"),
-		Pre(`HTTP/1.1 POST {host}/api/timesheets/{account}/{yyyymm}.timesheet
+
+		Section(
+			H2("Timesheets"),
+			P(
+				``,
+			),
+			H3("Create or update"),
+			Pre(`HTTP/1.1 POST {host}/api/timesheets/{account}/{yyyymm}.timesheet
 Authorization: {key}
 
 -- body contains timesheet --`),
 
-		H3("Read specific timesheet"),
-		Pre(`HTTP/1.1 GET {host}/api/timesheets/{account}/{yyyymm}.timesheet
+			H3("Read specific timesheet"),
+			Pre(`HTTP/1.1 GET {host}/api/timesheets/{account}/{yyyymm}.timesheet
 Authorization: {key}`),
-		"Responds with timesheet",
+			"Responds with timesheet",
 
-		H3("List timesheets of a specific user"),
-		Pre(`HTTP/1.1 GET {host}/api/timesheets/{account}/
+			H3("List timesheets of a specific user"),
+			Pre(`HTTP/1.1 GET {host}/api/timesheets/{account}/
 Authorization: {key}`),
-		`Responds with json {"timesheets": []}`,
+			`Responds with json {"timesheets": []}`,
+		),
+
+		Section(
+			H2("Timesheet file format"),
+			P("Timesheets are plain text and are specific to year and month"),
+			Pre(Class("timesheet"),
+				`2015 June
+---------
+23  1 Mon 8
+    2 Tue 8
+    3 Wed 8
+    4 Thu 8
+    5 Fri 6 (-2 flex) Ended work 2 hours early, felt sick.
+    6 Sat
+    7 Sun
+24  8 Mon 8
+    9 Tue 8
+   10 Wed 8
+   11 Thu 8 (7 conference) (1 travel)
+   12 Fri 8
+   13 Sat
+   14 Sun
+25 15 Mon 8
+   16 Tue 8
+   17 Wed 8:30 (0:30 flex)
+   18 Thu 8
+   19 Fri 8
+   20 Sat
+   21 Sun
+26 22 Mon 8
+   23 Tue 8
+   24 Wed 8
+   25 Thu 8
+   26 Fri 8
+   27 Sat
+   28 Sun
+27 29 Mon 8
+   30 Tue 8`,
+			),
+		),
 	)
 
 	return NewPage(
@@ -81,12 +126,20 @@ func theme() *CSS {
 		"padding: 1em 1em 2em 1em",
 		"min-height: 300",
 	)
+	css.Style("section",
+		"margin-bottom: 5em",
+	)
 	css.Style("pre",
 		"margin-left: 1em",
 	)
 	css.Style("footer",
 		"border-top: 1px solid #727272",
 		"padding: 0.6em 0.6em",
+	)
+	css.Style(".timesheet",
+		"border: 1px #e2e2e2 dotted",
+		"padding: 1em 1em",
+		"background-color: #ffffe6",
 	)
 	return css
 }
