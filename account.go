f9c8200f493a3e48aa52d0edef5456ebde2a8753
Gregory Vincic <g@7de.se>
2020-06-05 12:22:19 +0200

    Implement tidio.Account implements permission.Account

--- account.go
+++ account.go
@@ -1,13 +1,31 @@
 package tidio
 
+import "github.com/preferit/tidio/internal/permission"
+
 func NewAccount(username, role string) *Account {
+	uid := 0
+	gid := 0
 	return &Account{
+		UID_:     uid,
+		Groups:   []int{gid},
 		Username: username,
 		Role:     role,
 	}
 }
 
 type Account struct {
+	UID_     int // _ suffix so we can implement permission.Account
+	Groups   []int
 	Username string
 	Role     string
+}
+
+func (a *Account) UID() int { return a.UID_ }
+func (a *Account) Member(gid int) error {
+	for _, group := range a.Groups {
+		if group == gid {
+			return nil
+		}
+	}
+	return permission.ErrMembership
 }
