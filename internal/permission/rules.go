66eb500cfab865242bfdfcd98668b7736dea3d71
Gregory Vincic <g@7de.se>
2020-06-05 10:33:32 +0200

    Define permission Rules interface

--- internal/permission/rules.go
+++ internal/permission/rules.go
@@ -2,7 +2,13 @@ package permission
 
 import "errors"
 
-var DefaultRules = &Rules{}
+var DefaultRules Rules = &FileRules{}
+
+type Rules interface {
+	ToRead(Resource, Account) error
+	ToWrite(Resource, Account) error
+	ToExec(Resource, Account) error
+}
 
 func ToRead(e Resource, a Account) error {
 	return DefaultRules.ToRead(e, a)
@@ -14,41 +20,6 @@ func ToWrite(e Resource, a Account) error {
 
 func ToExec(e Resource, a Account) error {
 	return DefaultRules.ToExec(e, a)
-}
-
-type Rules struct{}
-
-func (*Rules) ToRead(e Resource, a Account) error {
-	switch {
-	case e.UID() == a.UID() && (e.Mode()&UserR == UserR):
-	case a.Member(e.GID()) == nil && (e.Mode()&GroupR == GroupR):
-	case e.Mode()&OtherR == OtherR:
-	default:
-		return ErrDenied
-	}
-	return nil
-}
-
-func (*Rules) ToWrite(e Resource, a Account) error {
-	switch {
-	case e.UID() == a.UID() && (e.Mode()&UserW == UserW):
-	case a.Member(e.GID()) == nil && (e.Mode()&GroupW == GroupW):
-	case e.Mode()&OtherW == OtherW:
-	default:
-		return ErrDenied
-	}
-	return nil
-}
-
-func (*Rules) ToExec(e Resource, a Account) error {
-	switch {
-	case e.UID() == a.UID() && (e.Mode()&UserX == UserX):
-	case a.Member(e.GID()) == nil && (e.Mode()&GroupX == GroupX):
-	case e.Mode()&OtherX == OtherX:
-	default:
-		return ErrDenied
-	}
-	return nil
 }
 
 var ErrDenied = errors.New("permission denied")
