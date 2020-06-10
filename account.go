a5d840359fc59977fb27a0f439de6a1c726e0e44
Gregory Vincic <g@7de.se>
2020-06-10 14:15:11 +0200

    Cleanup account

--- account.go
+++ account.go
@@ -1,19 +1,11 @@
 package tidio
 
 func NewAccount(username, role string) *Account {
-	uid := 0
-	gid := 0
 	return &Account{
-		UID_:     uid,
-		Groups:   []int{gid},
 		Username: username,
-		Role:     role,
 	}
 }
 
 type Account struct {
-	UID_     int // _ suffix so we can implement permission.Account
-	Groups   []int
 	Username string
-	Role     string
 }
