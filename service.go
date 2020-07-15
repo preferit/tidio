9332679abf5f95f5637521dbf76c2e2fb0248672
Gregory Vincic <g@7de.se>
2020-07-15 15:51:02 +0200

    Cleanup

--- service.go
+++ service.go
@@ -1,108 +1,22 @@
 package tidio
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
 	"io/ioutil"
 	"net/http"
-	"path"
-	"sync"
 
 	"github.com/gregoryv/fox"
-	"github.com/gregoryv/nugo"
 )
 
 func NewService() *Service {
 	s := &Service{
-		warn:    fox.NewSyncLog(ioutil.Discard).Log,
-		store:   make([]*Resource, 0),
-		nextUID: 100,
-		nextGID: 100,
+		warn: fox.NewSyncLog(ioutil.Discard).Log,
 	}
-	root := Account{Username: "root"}
-	s.AddUser(&root)
 	return s
 }
 
 type Service struct {
 	warn func(...interface{})
-
-	next    sync.Mutex
-	store   []*Resource
-	nextUID uint
-	nextGID uint
 }
-
-func (me *Service) AddUser(account *Account) error {
-	if account.Username == "" {
-		return fmt.Errorf("AddUser: empty username")
-	}
-	account.Ring = nugo.NewRing(me.nextIDs())
-	filename := fmt.Sprintf("%s.account", account.Username)
-	me.AddResource(&Resource{
-		Seal:   account.Seal(),
-		Path:   filename,
-		Entity: account,
-	})
-	// home dir
-	me.AddResource(&Resource{
-		Seal: account.Seal(),
-		Path: "/home/" + account.Username,
-	})
-	return nil
-}
-
-func (me *Service) Mkdir(dir string, a *Account) error {
-	var parent Resource
-	if err := me.Find(&parent, path.Dir(dir)); err != nil {
-		return err
-	}
-	perm := nugo.Permission{}
-
-	r := Resource{
-		Seal: a.Seal(),
-		Path: dir,
-	}
-	if err := perm.ToCreate(&parent, &r, a); err != nil {
-		return err
-	}
-	me.AddResource(&r)
-	return nil
-}
-
-// AddResource adds a resource with no premission control
-func (me *Service) AddResource(r *Resource) {
-	me.store = append(me.store, r)
-}
-
-func (me *Service) Find(resource *Resource, filename string) error {
-	for _, r := range me.store {
-		if r.Path == filename {
-			*resource = *r
-			return nil
-		}
-	}
-	return fmt.Errorf("Find: %s not found", filename)
-}
-
-func (me *Service) WriteTo(w io.Writer) error {
-	return json.NewEncoder(w).Encode(map[string]interface{}{
-		"resources": me.store,
-	})
-}
-
-func (me *Service) nextIDs() (uid uint, gid uint) {
-	me.next.Lock()
-	uid = me.nextUID
-	gid = me.nextGID
-	me.nextUID++
-	me.nextGID++
-	me.next.Unlock()
-	return
-}
-
-// ----------------------------------------
 
 func (me *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
--- service_test.go
+++ service_test.go
@@ -1,62 +1,14 @@
 package tidio
 
 import (
-	"bytes"
 	"testing"
 
 	"github.com/gregoryv/asserter"
-	"github.com/gregoryv/ex"
 )
 
-func TestService(t *testing.T) {
-	var (
-		assert  = asserter.New(t)
-		ok, bad = assert().Errors()
-		service = NewService()
-		john    = Account{Username: "john"}
-	)
-	ok(service.AddUser(&john))
-	ok(service.Mkdir("/home/john/tmp", &john))
-	bad(service.Mkdir("/haha", &john))
-}
-
-func TestService_AddUser(t *testing.T) {
-	var (
-		service = NewService()
-		assert  = asserter.New(t)
-		ok, bad = assert().Errors()
-	)
-	ok(service.AddUser(&Account{Username: "something"}))
-	bad(service.AddUser(&Account{}))
-}
-
-func TestService_WriteTo(t *testing.T) {
-	var (
-		assert  = asserter.New(t)
-		service = NewService()
-		buf     bytes.Buffer
-		nice    = ex.NewJsonWriter()
-	)
-	nice.Out = &buf
-	service.WriteTo(nice)
-	got := buf.String()
-	assert().Contains(got, "resources")
-	assert().Contains(got, "Path")
-	assert().Contains(got, "Entity")
-	assert().Contains(got, "Mode")
-	assert().Contains(got, "UID")
-	assert().Contains(got, "GID")
-	assert().Contains(got, "Username")
-	assert().Contains(got, `"root"`)
-}
-
-// ----------------------------------------
-
 func TestService_ServeHTTP(t *testing.T) {
-	var (
-		assert  = asserter.New(t)
-		service = NewService()
-		exp     = assert().ResponseFrom(service)
-	)
+	service := NewService()
+	assert := asserter.New(t)
+	exp := assert().ResponseFrom(service)
 	exp.StatusCode(200, "GET", "/api")
 }
