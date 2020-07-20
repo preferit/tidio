ecf3ff1eedc13d5c624d4f7ddadb34aab2813645
Gregory Vincic <g@7de.se>
2020-07-20 08:33:36 +0200

    Add /api/timesheets

--- service.go
+++ service.go
@@ -5,19 +5,60 @@ import (
 	"net/http"
 
 	"github.com/gregoryv/fox"
+	"github.com/gregoryv/go-timesheet"
+	"github.com/gregoryv/rs"
 )
 
 func NewService() *Service {
-	s := &Service{
+	sys := rs.NewSystem()
+	asRoot := rs.Root.Use(sys)
+	asRoot.Exec("/bin/mkdir /api")
+	asRoot.Exec("/bin/mkdir /api/timesheets")
+
+	// default templates
+	res, _ := asRoot.Create("/api/timesheets/202001.timesheet")
+	timesheet.Render(res, 2020, 1, 8)
+	res.Close()
+
+	return &Service{
 		warn: fox.NewSyncLog(ioutil.Discard).Log,
+		sys:  sys,
 	}
-	return s
 }
 
 type Service struct {
 	warn func(...interface{})
+
+	sys *rs.System
+}
+
+// SetLogger
+func (me *Service) SetLogger(log fox.Logger) {
+	me.warn = fox.NewFilterEmpty(log).Log
 }
 
 func (me *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
+	acc := me.authorize()
+	asAcc := acc.Use(me.sys)
+
+	// Check resource access permissions
+	_, err := asAcc.Stat(r.URL.Path)
+	if acc == rs.Anonymous && err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	// Serve the specific method
+	switch r.Method {
+	case "GET":
+		asAcc.Fexec(w, "/bin/ls", "-json", "-json-name", "resources", r.URL.Path)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+// authorize
+func (me *Service) authorize() *rs.Account {
+	acc := rs.Anonymous
+	return acc
 }
--- service_test.go
+++ service_test.go
@@ -1,14 +1,25 @@
 package tidio
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/gregoryv/asserter"
 )
 
 func TestService_ServeHTTP(t *testing.T) {
-	service := NewService()
+	srv := NewService()
 	assert := asserter.New(t)
-	exp := assert().ResponseFrom(service)
+	exp := assert().ResponseFrom(srv)
 	exp.StatusCode(200, "GET", "/api")
+	exp.StatusCode(405, "X", "/api")
+	exp.BodyIs(`{"resources":[{"name": "timesheets"}]}`, "GET", "/api")
+}
+
+func TestService_anonymousAccess(t *testing.T) {
+	srv := NewService()
+	srv.SetLogger(t)
+	assert := asserter.New(t)
+	exp := assert().ResponseFrom(srv)
+	exp.StatusCode(http.StatusUnauthorized, "GET", "/api/timesheets")
 }
