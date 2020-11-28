59c79c6b876ca7a7c576e839e9d78d14e6ce2836
Gregory Vincic <g@7de.se>
2020-11-28 09:41:24 +0100

    Add NewTrace

--- trace.go
+++ trace.go
@@ -10,15 +10,22 @@ import (
 )
 
 func newTrace(dst fox.Logger, r *http.Request) (trace fox.Logger, cleanup func()) {
+	trace, cl := NewTrace(dst)
+	return trace, func() {
+		cl(r.Method, r.URL)
+	}
+}
+
+func NewTrace(dst fox.Logger) (trace fox.Logger, cleanup func(...interface{})) {
 	var buf bytes.Buffer
 	l := log.New(&buf, "", log.Lshortfile)
 	trace = fox.LoggerFunc(func(v ...interface{}) {
 		l.Output(3, fmt.Sprint(v...))
 	})
-	cleanup = func() {
+	cleanup = func(v ...interface{}) {
 		if buf.Len() > 0 {
 			dst.Log("------------------------------")
-			dst.Log(r.Method, r.URL)
+			dst.Log(v...)
 			dst.Log(buf.String())
 			dst.Log("------------------------------")
 		}
