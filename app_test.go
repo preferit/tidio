b5a2d45a1fc9ffe92dfca2873ba17d537f3598fc
Gregory Vincic <g@7de.se>
2020-12-04 16:09:37 +0100

    Test failure to open file

--- app_test.go
+++ app_test.go
@@ -1,7 +1,9 @@
 package tidio
 
 import (
+	"io/ioutil"
 	"net/http"
+	"os"
 	"testing"
 
 	"github.com/gregoryv/fox"
@@ -62,6 +64,29 @@ func Test_state_file_cannot_be_written(t *testing.T) {
 	app.ListenAndServe = noopListenAndServe
 
 	cmd := wolf.NewTCmd("x", "-state", "/var/no-such")
+	defer cmd.Cleanup()
+	if app.Run(cmd) == 0 {
+		t.Error(cmd.Out.String(), cmd.Err.String())
+	}
+}
+
+func Test_state_file_cannot_be_read(t *testing.T) {
+	app := NewApp()
+	app.SetLogger(t)
+	app.ListenAndServe = noopListenAndServe
+
+	f, err := ioutil.TempFile("", "tidio")
+	if err != nil {
+		t.Fatal(err)
+	}
+	f.Write([]byte{'x'})
+	f.Close()
+	os.Chmod(f.Name(), 0000)
+	defer func() {
+		os.Chmod(f.Name(), 0644)
+		os.RemoveAll(f.Name())
+	}()
+	cmd := wolf.NewTCmd("x", "-state", f.Name())
 	defer cmd.Cleanup()
 	if app.Run(cmd) == 0 {
 		t.Error(cmd.Out.String(), cmd.Err.String())
