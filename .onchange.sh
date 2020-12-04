dc5e752bc952fa3d52095a180febc84f65f72efc
Gregory Vincic <g@7de.se>
2020-12-04 08:06:59 +0100

    Install all cmd's onchange

--- .onchange.sh
+++ .onchange.sh
@@ -13,9 +13,8 @@ esac
 
 #run="-run=hacks"
 go build ./...
-go test -coverprofile /tmp/c.out $run ./... 
-uncover -min 90 /tmp/c.out
-go install ./cmd/tidup
-go build -o /home/gregory/bin/tidio ./cmd/tidio
+go test -coverprofile /tmp/tidio.tprof $run ./...
+uncover -min 90 /tmp/tidio.tprof
+go install ./cmd/...
 killall tidio
-echo -e "\033[42m  \e[0m"
+
