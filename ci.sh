10d8b1ec63de68ebc8d4fd4243df2b23888223c5
Gregory Vincic <g@7de.se>
2021-11-13 15:19:55 +0100

    Move build steps from github workflow to ci.sh

--- ci.sh
+++ ci.sh
@@ -3,9 +3,19 @@
 # This script is meant to provide an easy way to build and test this
 # project either from CI pipeline or on a developer computer.
 
+dist=/tmp/tidio
+
 case $1 in
+    s|setup)
+        go install github.com/gregoryv/stamp/cmd/stamp
+	go install github.com/gregoryv/uncover/cmd/uncover
+	;;
     b|build)
-	go build ./...
+	mkdir -p $dist
+        go generate ./...
+        go build -o $dist/tidio ./cmd/tidio
+        cp -r ./systemd.service nginx.conf $dist
+        cp install.sh $dist
 	;;
     t|test)
 	#export group=integration
@@ -20,8 +30,11 @@ case $1 in
 	go install ./cmd/...
 	sudo systemctl start tidio
 	;;
+    c|clean)
+	rm -rf $dist
+	;;
     *)
-	echo "Usage: $0 [b]uild|[t]est|[u]ncover|[i]nstall"
+	echo "Usage: $0 [s]etup|[b]uild|[t]est|[u]ncover|[i]nstall|[c]lean"
 	echo ""
 	echo "$0 build test uncover"
 	echo "$0 b t u"
