2dc96f493c69200eb4d7d48296b6a06abd969a23
Gregory Vincic <g@7de.se>
2021-11-15 11:59:25 +0100

    Simplify integration tests

--- integration_test.go
+++ integration_test.go
@@ -6,7 +6,25 @@ import (
 	"testing"
 )
 
-func Test_integration(t *testing.T) {
+func Test_read_root(t *testing.T) {
+	api, log := integration(t)
+	resp := api.ReadTimesheet("/").MustSend()
+	if resp.Status != "200 OK" {
+		t.Error(resp.Status, "\n", log.FlushString())
+	}
+}
+
+func Test_read_unknown(t *testing.T) {
+	api, log := integration(t)
+	resp := api.ReadTimesheet("/api/jibberish").MustSend()
+	got, exp := resp.Status, "401 Unauthorized"
+	if got != exp {
+		t.Errorf("%s\n%q != %q", log.FlushString(), got, exp)
+	}
+}
+
+func integration(t *testing.T) (*API, *LogPrinter) {
+	t.Helper()
 	if !strings.Contains(os.Getenv("group"), "integration") {
 		t.SkipNow()
 	}
@@ -14,13 +32,5 @@ func Test_integration(t *testing.T) {
 		api = NewAPI("http://localhost:13001")
 		log = Register(api).Buf()
 	)
-	resp := api.ReadTimesheet("/").MustSend()
-	if resp.Status != "200 OK" {
-		t.Error(resp.Status, "\n", log.FlushString())
-	}
-
-	resp = api.ReadTimesheet("/api/jibberish").MustSend()
-	if resp.Status != "401 Unauthorized" {
-		t.Errorf("%s\n%s", resp.Status, log.FlushString())
-	}
+	return api, log
 }
