f7f3f1ba514cacc05a2625f8836a6aefd47114e6
Gregory Vincic <g@7de.se>
2021-11-16 06:22:17 +0100

    Move config closer to htapi

--- htapi.go
+++ htapi.go
@@ -5,6 +5,8 @@ import (
 	"encoding/base64"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"time"
 
@@ -291,4 +293,79 @@ func (me *API) warn(err error) {
 		return
 	}
 	Log(me).Info(err)
+}
+
+// ----------------------------------------
+
+func NewConfig() *Config {
+	c := &Config{
+		activeLoggers: make(map[interface{}]*LogPrinter),
+	}
+	c.SetOutput(ioutil.Discard)
+	return c
+}
+
+// Config holds reference to loggers for various objects.
+type Config struct {
+	out io.Writer
+
+	debug bool
+
+	activeLoggers map[interface{}]*LogPrinter
+}
+
+// SetOutput
+func (me *Config) SetOutput(w io.Writer) { me.out = w }
+
+// Debug
+func (me *Config) Debug() bool { return me.debug }
+func (me *Config) SetDebug(v bool) {
+	me.debug = v
+}
+
+// Unregister removes the previously registered item if any.
+func (me *Config) Unregister(v interface{}) {
+	delete(me.activeLoggers, v)
+}
+
+func (me *Config) Log(v interface{}) *LogPrinter {
+	l, found := me.activeLoggers[v]
+	if !found {
+		return nolog
+	}
+	return l
+}
+
+// Register creates a LogPrinter for the given objects.
+// The registered LogPrinter is later retrieved with Config.Log(v)
+func (me *Config) Register(v ...interface{}) *LogPrinter {
+	if len(v) == 0 {
+		panic("missing values in MLog")
+	}
+
+	first := v[0]
+	l := NewLogPrinter(me.out)
+	me.activeLoggers[first] = l
+	for _, other := range v[1:] {
+		me.activeLoggers[other] = l
+	}
+	return l
+}
+
+// ----------------------------------------
+
+// Wrappers for default config Conf
+
+func Register(v ...interface{}) *LogPrinter {
+	return Conf.Register(v...)
+}
+func Log(v interface{}) *LogPrinter { return Conf.Log(v) }
+func Unregister(v interface{})      { Conf.Unregister(v) }
+
+var Conf = NewConfig()
+
+// ----------------------------------------
+
+var nolog = &LogPrinter{
+	lgr: log.New(ioutil.Discard, "", 0),
 }
--- htapi_test.go
+++ htapi_test.go
@@ -215,6 +215,31 @@ func TestBasicAuth_Set(t *testing.T) {
 	})
 }
 
+func Test_loggers(t *testing.T) {
+	log := Register(t).Buf()
+	defer Unregister(t)
+
+	somefunc(t) // should log
+	Conf.Unregister(t)
+	somefunc(t) // no logger registered
+
+	got := log.FlushString()
+	if strings.Count(got, "hello") != 1 {
+		t.Errorf("cached log\n%s", got)
+		t.Error("writes", log.writes)
+	}
+}
+
+func Test_Register_panics(t *testing.T) {
+	defer catchPanic(t)
+	Register()
+}
+
+func somefunc(t *testing.T) {
+	Log(t).Info("hello")
+	Log(t).Info("world")
+}
+
 // ----------------------------------------
 
 func Test_read_root(t *testing.T) {
